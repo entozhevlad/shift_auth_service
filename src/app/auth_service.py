b7a02ddf5c408c16b273a34ecbfdf068ca878973
Vladislav Zarubin <vladislavzarubin113@gmail.com>
2024-07-14 14:43:58 +0000

    Измененеия по линтеру

--- src/app/auth_service.py
+++ src/app/auth_service.py
@@ -1,3 +1,4 @@
+# src/app/auth_service.py
 import datetime
 from dataclasses import dataclass
 from typing import Dict, Optional
@@ -10,9 +11,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 @dataclass
 class User:
-    """
-    Класс, представляющий пользователя с именем, паролем и токеном.
-    """
+    """Класс, представляющий пользователя с именем, паролем и токеном."""
 
     username: str
     password: str
@@ -20,34 +19,14 @@ class User:
 
 
 class AuthService:
-    """
-    Сервис аутентификации пользователей с использованием JWT.
-    """
+    """Сервис аутентификации пользователей с использованием JWT."""
 
     def __init__(self):
-        """
-        Конструктор класса
-        """
-
+        """Конструктор класса."""
         self.users: Dict[str, User] = {}
 
-    def _generate_token(self, username: str) -> str:
-        """
-        Генерирует JWT токен для указанного имени пользователя.
-
-        """
-
-        payload = {
-            'username': username,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
-        }
-        return jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-
     def register(self, username: str, password: str) -> Optional[str]:
-        """
-        Регистрирует нового пользователя с указанным именем пользователя и паролем.
-        """
-
+        """Регистрирует нового пользователя. Возвращает JWT токен."""
         if username in self.users:
             return None
         token = self._generate_token(username)
@@ -56,12 +35,17 @@ class AuthService:
         return token
 
     def authenticate(self, username: str, password: str) -> Optional[str]:
-        """
-         Аутентифицирует пользователя по имени и паролю.
-        """
-
+        """Аутентифицирует пользователя по имени и паролю."""
         user = self.users.get(username)
         if not user or user.password != password:
             return None
         user.token = self._generate_token(username)
         return user.token
+
+    def _generate_token(self, username: str) -> str:
+        """Генерирует JWT токен для указанного имени пользователя."""
+        payload = {
+            'username': username,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1),
+        }
+        return jwt.encode(payload, SECRET_KEY, algorithm='HS256')
--- src/app/main.py
+++ src/app/main.py
@@ -6,30 +6,32 @@ logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    """
-    Функция приложения для демонстрации работы AuthService
-    """
+    """Функция приложения для демонстрации работы AuthService."""
     auth_service = AuthService()
 
-    username = "user"
-    password = "password"
+    username = 'user'
+    test_pass = 'qwerty735'
 
     # Регистрация пользователя
-    token = auth_service.register(username, password)
+    token = auth_service.register(username, test_pass)
     if token:
-        logging.info(f"Пользователь успешно зарегистрирован. Токен: {token}")
+        logging.info(
+            'Пользователь успешно зарегистрирован. Токен: {0}'.format(token),
+        )
     else:
-        logging.info("Пользователь с таким именем уже существует.")
+        logging.info('Пользователь с таким именем уже существует.')
 
     # Аутентификация пользователя
-    auth_token = auth_service.authenticate(username, password)
+    auth_token = auth_service.authenticate(username, test_pass)
     if auth_token:
         logging.info(
-            f"Пользователь успешно аутентифицирован. Обновленный токен: {auth_token}")
+            'Пользователь аутентифицирован. Токен: {0}'.format(auth_token),
+        )
     else:
         logging.info(
-            "Ошибка аутентификации: неверное имя пользователя или пароль")
+            'Ошибка аутентификации: неверное имя пользователя или пароль',
+        )
 
 
-if __name__ == "__main__":
-    main()  # комментарий
+if __name__ == '__main__':
+    main()
